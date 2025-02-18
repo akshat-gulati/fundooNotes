06655d8354da1101cfe3b857b01eaf272d52c467
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2025-02-19 02:55:27 +0530

    done

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,52 +1,54 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const SearchBar = () => {
   return (
     <View style={styles.container}>
-        <View style={styles.leftmost}>
-          <Image style={styles.icon} source={require('../Assets/line.3.horizontal.png')}/>
-        <Text style={{ fontSize:17,color:'#83848A', alignSelf:'center', marginLeft:20}}>Search Your Notes</Text>
-        </View>
-      
+      <View style={styles.leftmost}>
+        <Image style={styles.icon} source={require('../Assets/line.3.horizontal.png')}/>
+        <Text style={styles.searchText}>Search Your Notes</Text>
+      </View>
       <View style={styles.rightmost}>
-      <Image style={[styles.icon, {tintColor: '#9C9FA5'}]} source={require('../Assets/rectangle.grid.1x2.png')} />
-        <Image style={[styles.icon, {marginLeft: 20}]} source={require('../Assets/person.crop.circle.png')} />
-
-
-
+        <Image style={[styles.icon, styles.gridIcon]} source={require('../Assets/rectangle.grid.1x2.png')} />
+        <Image style={[styles.icon, styles.profileIcon]} source={require('../Assets/person.crop.circle.png')} />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
 
 const styles = StyleSheet.create({
-    container:{
-        borderWidth:1,
-        backgroundColor:'#2B2B2F',
-        borderRadius:10,
-        padding:15,
-        flexDirection:'row',
-        justifyContent:'space-between'
-        
-
-    },
-    leftmost:{
-      flexDirection:'row'
-
-    },
-    icon:{
-      tintColor:'white',
-      resizeMode:'contain',
-      height:25,
-      width:25
-
-
-    },
-    rightmost:{
-      flexDirection:'row',
-
-    }
-})
+  container: {
+    borderWidth: 1,
+    backgroundColor: '#2B2B2F',
+    borderRadius: 10,
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftmost: {
+    flexDirection: 'row',
+  },
+  icon: {
+    tintColor: 'white',
+    resizeMode: 'contain',
+    height: 25,
+    width: 25,
+  },
+  rightmost: {
+    flexDirection: 'row',
+  },
+  searchText: {
+    fontSize: 17,
+    color: '#83848A',
+    alignSelf: 'center',
+    marginLeft: 20,
+  },
+  gridIcon: {
+    tintColor: '#9C9FA5',
+  },
+  profileIcon: {
+    marginLeft: 20,
+  },
+});
